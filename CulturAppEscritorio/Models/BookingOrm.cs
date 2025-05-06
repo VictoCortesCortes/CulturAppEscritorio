f69e8daf77f4c5da604ddb5798ab91998d4d072e
Victor <vcortesc2324@politecnics.barcelona>
2025-05-06 18:50:35 +0200

    Creación de clases para mostrar los datos correctamente

---
+++ CulturAppEscritorio/Models/BookingOrm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulturAppEscritorio.Models
+{
+    public class BookingComplete
+    {
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public int event_id { get; set; }
+        public string event_name { get; set; }
+        public int quantity { get; set; }
+    }
+    public static class BookingOrm
+    {
+    }
+}
---
+++ CulturAppEscritorio/Models/EventsOrm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CulturAppEscritorio.Models
+{
+    public class EventsComplete
+    {
+        public int event_id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+        public int capacity { get; set; }
+        public int price { get; set; }
+        public int type_id { get; set; }
+        public string type { get; set; }
+        public int room_id { get; set; }
+        public string room { get; set; }
+    }
+    public static class EventsOrm
+    {
+    }
+}
