05a8785ce99b8ce1193dc4cc52043af585ea2cc9
Victor <vcortesc2324@politecnics.barcelona>
2025-05-09 16:34:12 +0200

    Try catch en TypeEventOrm

--- CulturAppEscritorio/Models/TypeEventOrm.cs
+++ CulturAppEscritorio/Models/TypeEventOrm.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CulturAppEscritorio.Models
@@ -7,19 +8,36 @@ namespace CulturAppEscritorio.Models
     {
         public static List<Type_event> SelectGlobal()
         {
-            List<Type_event> _type =
-                        (from type in Orm.bd.Type_event
-                         select type).ToList();
-            return _type;
+            try
+            {
+                List<Type_event> _type =
+                    (from type in Orm.bd.Type_event
+                     select type).ToList();
+                return _type;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en TypeEventOrm SelectGlobal: " + ex.Message);
+                return new List<Type_event>();
+            }
         }
 
         public static Type_event SelectByName(string name)
         {
-            Type_event _type =
-                        (from type in Orm.bd.Type_event
-                         where type.name == name
-                         select type).FirstOrDefault();
-            return _type;
+            try
+            {
+                Type_event _type =
+                    (from type in Orm.bd.Type_event
+                     where type.name == name
+                     select type).FirstOrDefault();
+                return _type;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en TypeEventOrm SelectByName: " + ex.Message);
+                return null;
+            }
         }
     }
+
 }
