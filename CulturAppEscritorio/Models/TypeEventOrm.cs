03c8826b0fc6b18353d642cdb3be01880c6fe6ee
Victor <vcortesc2324@politecnics.barcelona>
2025-05-09 16:45:31 +0200

    Comentarios en TypeEventOrm

--- CulturAppEscritorio/Models/TypeEventOrm.cs
+++ CulturAppEscritorio/Models/TypeEventOrm.cs
@@ -6,6 +6,10 @@ namespace CulturAppEscritorio.Models
 {
     public static class TypeEventOrm
     {
+        /// <summary>
+        /// Obtiene todos los tipos de evento de la base de datos.
+        /// </summary>
+        /// <returns>Lista de objetos <see cref="Type_event"/> que representan los tipos de evento en la base de datos.</returns>
         public static List<Type_event> SelectGlobal()
         {
             try
@@ -18,10 +22,15 @@ namespace CulturAppEscritorio.Models
             catch (Exception ex)
             {
                 Console.WriteLine("Error en TypeEventOrm SelectGlobal: " + ex.Message);
-                return new List<Type_event>();
+                return new List<Type_event>();  // Retorna una lista vacía en caso de error
             }
         }
 
+        /// <summary>
+        /// Obtiene un tipo de evento específico por su nombre.
+        /// </summary>
+        /// <param name="name">El nombre del tipo de evento a buscar.</param>
+        /// <returns>Un objeto <see cref="Type_event"/> que representa el tipo de evento, o null si no se encuentra.</returns>
         public static Type_event SelectByName(string name)
         {
             try
@@ -39,5 +48,4 @@ namespace CulturAppEscritorio.Models
             }
         }
     }
-
 }
