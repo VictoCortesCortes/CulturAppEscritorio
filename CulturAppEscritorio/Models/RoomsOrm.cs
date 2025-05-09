638e154256d54e6cde5ac15b6ae4c647fdff2d75
Victor <vcortesc2324@politecnics.barcelona>
2025-05-09 16:31:58 +0200

    Try catch en RoomsOrm

--- CulturAppEscritorio/Models/RoomsOrm.cs
+++ CulturAppEscritorio/Models/RoomsOrm.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CulturAppEscritorio.Models
@@ -7,37 +8,68 @@ namespace CulturAppEscritorio.Models
     {
         public static List<Rooms> SelectGlobal()
         {
-            List<Rooms> _rooms =
-                        (from room in Orm.bd.Rooms
-                         select room).ToList();
-            return _rooms;
+            try
+            {
+                List<Rooms> _rooms =
+                    (from room in Orm.bd.Rooms
+                     select room).ToList();
+                return _rooms;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en RoomsOrm SelectGlobal: " + ex.Message);
+                return new List<Rooms>();
+            }
         }
 
         public static Rooms SelectByName(string name)
         {
-            Rooms _rooms =
-                        (from room in Orm.bd.Rooms
-                         where room.name == name
-                         select room).FirstOrDefault();
-            return _rooms;
+            try
+            {
+                Rooms _rooms =
+                    (from room in Orm.bd.Rooms
+                     where room.name == name
+                     select room).FirstOrDefault();
+                return _rooms;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en RoomsOrm SelectByName: " + ex.Message);
+                return null;
+            }
         }
 
         public static void Insert(Rooms room)
         {
-            Orm.bd.Rooms.Add(room);
-            Orm.bd.SaveChanges();
+            try
+            {
+                Orm.bd.Rooms.Add(room);
+                Orm.bd.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en RoomsOrm Insert: " + ex.Message);
+            }
         }
 
         public static void Update(Rooms room)
         {
-            Rooms _room = Orm.bd.Rooms.Find(room.id);
-            if (_room != null)
+            try
             {
-                _room.name = room.name;
-                _room.description = room.description;
-                _room.size = room.size;
-                Orm.bd.SaveChanges();
+                Rooms _room = Orm.bd.Rooms.Find(room.id);
+                if (_room != null)
+                {
+                    _room.name = room.name;
+                    _room.description = room.description;
+                    _room.size = room.size;
+                    Orm.bd.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en RoomsOrm Update: " + ex.Message);
             }
         }
     }
+
 }
